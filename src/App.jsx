b47e0af2ad425d0861ba3f64c0343ceadee909e4
Matias-Alef <newlifemakeup@gmail.com>
2022-03-01 19:33:45 -0300

    First commit login api

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import HomePage from './homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
